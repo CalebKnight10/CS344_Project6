64ddbaa0c4c51455aec1e9458da9e233cb836703
Caleb Knight <knightca@oregonstate.edu>
2022-02-14 12:42:26 -0800

    Began adding in instructions adn provided code from the exploration

--- myalloc.c
+++ myalloc.c
@@ -1,3 +1,44 @@
 // Caleb Knight
 // CS 344
 // Programming Exercise in C to write an allocator 
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ALIGNMENT 16   // Must be power of 2
+#define GET_PAD(x) ((ALIGNMENT - 1) - (((x) - 1) & (ALIGNMENT - 1)))
+#define PADDED_SIZE(x) ((x) + GET_PAD(x))
+#define PTR_OFFSET(p, offset) ((void*)((char *)(p) + (offset)))
+
+
+struct block *head = NULL;
+
+struct block {
+    struct block *next;
+    int size;     // Bytes
+    int in_use;   // Boolean
+}
+
+void print_data(void)
+{
+    struct block *b = head;
+
+    if (b == NULL) {
+        printf("[empty]\n");
+        return;
+    }
+
+    while (b != NULL) {
+        // Uncomment the following line if you want to see the pointer values
+        //printf("[%p:%d,%s]", b, b->size, b->in_use? "used": "free");
+        printf("[%d,%s]", b->size, b->in_use? "used": "free");
+        if (b->next != NULL) {
+            printf(" -> ");
+        }
+
+        b = b->next;
+    }
+
+    printf("\n");
+}
